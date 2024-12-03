73e61f2c776c66df1efb71508c487d1969827330
unknown <mahmodmorad144@example.com>
2024-12-03 15:10:03 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final Int date;
+  final String color;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
+}
