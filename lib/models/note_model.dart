62cc35c730faf0dc079e6c4eeb4d0ce714c79e59
unknown <mahmodmorad144@example.com>
2024-12-04 17:11:52 +0200

    create type adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,18 @@
 import 'dart:ffi';
 
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final Int date;
+  @HiveField(3)
   final String color;
 
   NoteModel(
@@ -11,5 +20,4 @@ class NoteModel {
       required this.subTitle,
       required this.date,
       required this.color});
-  void name() {}
 }
